945703f5de05457f74885bfe5e4b581478955719
Ilia Orlov <66363651+illfixit@users.noreply.github.com>
2025-06-06 20:34:30 +0000

    fix zoom

--- src/components/Scene.tsx
+++ src/components/Scene.tsx
@@ -33,7 +33,7 @@ const Scene: React.FC = () => {
       <ambientLight intensity={0.3} />
       <directionalLight position={[6, 5, 1]} intensity={1} castShadow />
       {/* Place camera at a 3-corner iso view */}
-      <OrthographicCamera makeDefault position={[10, 10, 10]} zoom={70} />
+      <OrthographicCamera makeDefault position={[10, 10, 10]} zoom={60} />
       <OrbitControls
         ref={controlsRef}
         onEnd={handleControlsEnd}
